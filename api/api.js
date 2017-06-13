0cfd4a54f86dda295ef0e3e8edfcd94548e60731
michael <michgboxy@gmail.com>
2017-06-13 07:04:46 -0700

    mouting the routers on the api

---
+++ api/api.js
@@ -0,0 +1,12 @@
+var express = require("express"),
+	api		= express.Router(),
+	adminRouter = require("./v1/admin/admin-router.js"),
+	authRouter	= require("./v1/auth/auth-router.js");
+
+	//mount the routers on the API
+
+	api.use("/admin",adminRouter);
+	api.use("/auth", authRouter);
+
+	modules.exports = api;
+
