4adf1131a0e439bd21545ca4cfa244f0c226608c
michael <michgboxy@gmail.com>
2017-06-14 07:56:47 -0700

    mounts the controller router on the api

--- api/api.js
+++ api/api.js
@@ -1,12 +1,14 @@
 var express = require("express"),
 	api		= express.Router(),
 	adminRouter = require("./v1/admin/admin-router.js"),
+	customerRouter = require("./v1/customer/customer-router.js"),
 	authRouter	= require("./v1/auth/auth-router.js");
 
 	//mount the routers on the API
 
 	api.use("/admin",adminRouter);
 	api.use("/auth", authRouter);
+	api.use("/customer", customerRouter);
 
 	module.exports = api;
 
