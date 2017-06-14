259640be4c999a11cc618816a061cf3b25159192
michael <michgboxy@gmail.com>
2017-06-13 21:20:41 -0700

    includes token in the admin signup for security

--- api/v1/admin/admin-controller.js
+++ api/v1/admin/admin-controller.js
@@ -1,57 +1,62 @@
-var	adminModel 	= require("./admin-model.js"),
+var adminModel	= require("./admin-model.js"),
 	auth		= require("../auth/auth.js");
 
 
-	exports.InterceptId = (req, res,next,id) => {
+
+	exports.InterceptId = (req, res, next, id) => {
 		var id = req.params.id;
-
-		adminModel.findById(id).then((data) => {
-			if(!data){ return next(new Error("can't find Id"));}
-
-			req.admin = data;
+		adminModel.findById(id).then((data)=> {
+			if(!data){ return next(new Error("cant find id"));}
+			data = req.admin;
 			next();
-
 		})
+
 	}
 
-	//register admin 
+
 	exports.RegisterAdmin = (req, res, next) =>{
 		var admin = req.body,
 			adminObj = new adminModel(admin);
 
-			adminObj.save((data) => {
-				if(!data){ return next(new Error("can't save admin"));}
-				res.status(200).json(data);
-			});
-	}
+			adminObj.save((err, data) => {
+				if(err){return next(new Error("can't register admin"));}
 
-	//get admin by Id
-	exports.FetchAllAdmin = (req, res, next) => {
-			if(!req.admin){return next(new Error("can't fetch admins"));}
-			res.status(200).json(req.body);
-
-	}
-	//delete admin
-	exports.RemoveAdmin = (req, res, next) => {
-		adminModel.findByIdAndRemove(id).then((data) => {
-			if(!data){ return next(new Error("can't delete admin"));}
-			res.status(200).json(data);
-		})
-	}
-
-	exports.FetchAdminById = (req, res, next) => {
-		adminModel.findById(id).then((data) => {
-			if(!data){ return next(new Error("can't fetch addmin"));}
-			res.status(200).json(data);
-		})
-	} 
-
-	exports.EditAdmin = (req, res, next) => {
-			var id = req.params.id;
-
-			adminModel.findByIdAndUpdate(id, req.body).then((data) => {
-				if(!data){ return next(new Error("can't update admin"));}
+				data = data.toObject();
+				var token = auth.signToken(data._id);
+				data['_token'] = token;
 				res.status(200).json(data);
 			})
 	}
 
+	exports.FetchAllAdmin =(req, res, next) => {
+		adminModel.find((err, data)=> {
+			if(err){return next(new Error("can't fetch Admins"));}
+			res.status(200).json(data);
+		})
+	}
+
+	exports.RemoveAdmin = (req, res, next) => {
+		var id =req.params.id;
+
+		adminModel.findByIdAndRemove(id).then((data)=> {
+			if(!data){ return next(new Error("can't delete admin"));}
+			res.status(200).json(data);
+		}, (err) =>{ return next(err);})
+	}
+
+	exports.EditAdmin = (req, res, next) =>{
+		var id = req.params.id;
+
+		adminModel.findByIdAndUpdate(id, req.body).then((data) => {
+			if(!data){return next(new Error("can't update admin"));}
+			res.status(200).json(data);
+		}, (err) => { return next(err);})
+	}
+
+	exports.FetchAdminById = (req, res, next) => {
+		var id = req.params.id;
+		adminModel.findById(id).then((data) => {
+			if(!data){ return next(new Error("can't fetch admin"));}
+			res.status(200).json(data);
+		}, (err) => {return next(err);})
+	}
