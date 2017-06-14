eaeeb9830740d615630b919923e9020193068a9b
michael <michgboxy@gmail.com>
2017-06-14 07:40:28 -0700

    controller to fetch and remove a customer

--- api/v1/customer/customer-controller.js
+++ api/v1/customer/customer-controller.js
@@ -16,6 +16,25 @@ var  cusModel = require("./customer-model.js"),
 
 	 		customerObj.save((err, data) => {
 	 			if(err){ return next( new Error("can't register customer"));}
+	 			data = data.toObject();
+	 			var token = auth.SignToken(data._id);
+	 			data["_token"] = token;
 	 			res.status(200).json(data);
 	 		})
+	 }
+
+	 exports.RemoveCustomer = (req, res, next) => {
+	 	var id = req.params.id;
+
+	 	cusModel.findByIdAndRemove(id).then((data) => {
+	 		if(!data){ return next(new Error("can't remove customer"));}
+	 		res.status(200).json(data);
+	 	},(err) => { return next(err);})
+	 }
+
+	 exports.FetchCustomer = (req, res, next) => {
+	 	cusModel.find((err, data) = > {
+	 		if(!data){ return next(new Error("can'tfind customer"));}
+	 		res.status(200).json(data);
+	 	})
 	 }
