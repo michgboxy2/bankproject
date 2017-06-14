0039af31f4017a5eb84fe7670a2a7bfc186d16ea
michael <michgboxy@gmail.com>
2017-06-13 21:24:33 -0700

    sets port listen

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-var express		= require("express"),
-	app			= express(),
-	server		= require("./server/server.js");
+var express 	= require("express"),
+	app			= require("./server/server.js");
+
+
+	app.use(express.static(__dirname + "/www"));
+
+
+
+	app.listen(3000, () => {
+		console.log("server started");
+	}); 
