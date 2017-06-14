38fde4e2f750dc9ed7bb0064447009dc27f7a328
michael <michgboxy@gmail.com>
2017-06-13 21:23:29 -0700

    auth signIn

--- api/v1/auth/auth-router.js
+++ api/v1/auth/auth-router.js
@@ -1,7 +1,9 @@
 var auth	= require("./auth.js"),
-controller  = require("/.auth-controller.js"),
+controller  = require("./auth-controller.js"),
 	express = require("express"),
 	router  = express.Router();
 
 	router.route("/")
-	.post(auth.VerifyUser, controller.signIn);
+	.post(auth.verifyUser, controller.signIn);
+
+	module.exports = router;
