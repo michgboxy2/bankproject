f152adab1afdf1989dee9ae201d2fd9b744133ea
michael <michgboxy@gmail.com>
2017-06-13 22:02:04 -0700

    controller to register customer

--- api/v1/customer/customer-controller.js
+++ api/v1/customer/customer-controller.js
@@ -1,5 +1,5 @@
 var  cusModel = require("./customer-model.js"),
-	 auth	  = require("../auth/auth-js");
+	 auth	  = require("../auth/auth.js");
 
 
 	 exports.InterceptId = (req, res,next,id) => {
@@ -8,4 +8,14 @@ var  cusModel = require("./customer-model.js"),
 	 		req.customer = data;
 	 		next();
 	 	}, (err) => { return next(err);})
+	 }
+
+	 exports.RegisterCustomer = (req, res, next) => {
+	 	var customer = req.body,
+	 		customerObj = new cusModel(customer);
+
+	 		customerObj.save((err, data) => {
+	 			if(err){ return next( new Error("can't register customer"));}
+	 			res.status(200).json(data);
+	 		})
 	 }
