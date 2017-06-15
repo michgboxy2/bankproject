553311402580bc3ff1ac1131fb959943f18a2a6d
michael <michgboxy@gmail.com>
2017-06-15 03:36:34 -0700

    sets customer account number to a 10digits random number

--- api/v1/customer/customer-model.js
+++ api/v1/customer/customer-model.js
@@ -9,16 +9,20 @@ var mongoose	= require("mongoose"),
 		lastname : {type: String, required: true},
 		email : {type: String, required: true, unique : true},
 		account_type : {type: String, required : true},
-		account_number : {type: Number, default : Math.random() * (000000000,999999999)},
+		account_number : {type: Number, default : Math.floor(Math.random() * (9999999999))},
 		account_balance : {type : Number, required : true},
 		password : {type : String, required : true},
 		admin_id : [{type: mongoose.Schema.Types.ObjectId, ref: 'admin'}]
 	});
 
 	customerSchema.pre('save', function(next){
+		//customerSchema.find({}).populate('admin').exec(callback);
+
 		this.password = this.encryptPassword(this.password);
 		next();
 	});
+
+
 
 	customerSchema.methods = {
 		authenticate : function(plaintext){
