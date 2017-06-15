def8e2d27637922fac7255a03bced36134cb48fd
michael <michgboxy@gmail.com>
2017-06-14 21:07:41 -0700

    making sure an admin is authenticated and signed in before a customer can be added, edited, deleted and fetched

---
+++ api/v1/customer/customer-router.js
@@ -0,0 +1,24 @@
+var express  = require("express"),
+	router   = express.Router(),
+	auth  	 = require("../auth/auth.js"),
+	control	 = require("../auth/auth-controller.js"),
+	controller = require("./customer-controller.js");
+
+
+	//mounting the controller that intercept IDs
+	router.param("id", controller.InterceptId);
+
+
+	//mounting the controller that register customer and the one that fetches all customer to their respective rout.
+	router.route("/")
+	.post(auth.verifyUser, control.signIn,controller.RegisterCustomer)
+	.get(auth.verifyUser, control.signIn, controller.FetchAllCustomers);
+
+
+	//mounting the delete, fetch and update controllers that requires an id;
+	router.route("/:id")
+	.delete(auth.verifyUser, control.signIn, controller.RemoveCustomer)
+	.get(auth.verifyUser, control.signIn, controller.FetchOneCustomer)
+	.put(auth.verifyUser, control.signIn, controller.EditCustomer);
+
+	module.exports = router;
