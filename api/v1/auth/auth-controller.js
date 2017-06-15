04b532ed66ef8ac3f3abee18b8bf76162071370a
michael <michgboxy@gmail.com>
2017-06-14 21:03:25 -0700

    admin verification before signIn

--- api/v1/auth/auth-controller.js
+++ api/v1/auth/auth-controller.js
@@ -2,6 +2,7 @@ var auth 	= require("./auth.js");
 
 
 	exports.signIn = (req, res, next) => {
+
 		var token = auth.signToken(req.admin._id);
-		res.status({_token : token});
+		res.status(200).json({_token : token});
 	}
