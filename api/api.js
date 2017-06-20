a2ba6663ca4381dc5a2e5d60fd036f8fe1502882
michael <michgboxy@gmail.com>
2017-06-20 13:45:33 -0700

    mounts the transaction router on the api

--- api/api.js
+++ api/api.js
@@ -2,7 +2,8 @@ var express = require("express"),
 	api		= express.Router(),
 	adminRouter = require("./v1/admin/admin-router.js"),
 	customerRouter = require("./v1/customer/customer-router.js"),
-	cusAuth		= 	 require("./v1/cus-auth-router.js"),
+	cusAuth		= 	 require("./v1/auth/cus-auth-router.js"),
+	transactionRouter =	 require("./v1/transaction/transaction-router.js"),
 	authRouter	= require("./v1/auth/auth-router.js");
 
 	//mount the routers on the API
@@ -11,6 +12,7 @@ var express = require("express"),
 	api.use("/auth", authRouter);
 	api.use("/customer", customerRouter);
 	api.use("/customerAuth", cusAuth);
+	api.use("/transaction", transactionRouter);
 
 	module.exports = api;
 
