f5efdaf7d75cc46c6384cb76cce17838ab8ad37d
michael <michgboxy@gmail.com>
2017-06-20 13:47:57 -0700

    removes the customer-auth controller mounted here

--- api/v1/auth/auth-router.js
+++ api/v1/auth/auth-router.js
@@ -1,12 +1,9 @@
 var auth	= 		require("./auth.js"),
-	cusauth = 		require("./cus-auth.js"),
-	cusController = require("./cus-auth-controller.js"),
 	controller  = 	require("./auth-controller.js"),
 	express = 		require("express"),
 	router  = 		express.Router();
 
 	router.route("/")
-	.post(auth.verifyUser, controller.signIn)
-	.post(cusauth.verifyCustomer, cusController.signIn);
+	.post(auth.verifyUser, controller.signIn);
 	
 	module.exports = router;
