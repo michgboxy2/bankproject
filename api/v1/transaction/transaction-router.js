b4dbcccc9c60967f0973c643b2e4d54f23dfaebf
michael <michgboxy@gmail.com>
2017-06-20 13:54:57 -0700

    mounts the transaction controllers on the router while checking for accessibility pass

--- api/v1/transaction/transaction-router.js
+++ api/v1/transaction/transaction-router.js
@@ -1,3 +1,13 @@
 var	 express = 		require("express"),
 	 router	 =		express.Router(),
+	 auth	 = 		require("../auth/cus-auth.js"),
 	 controller =   require("./transaction-controller.js");
+
+	 router.route("/")
+	 .post(auth.decodeToken, controller.postTransaction)
+	 .get(auth.decodeToken, controller.FetchTransaction);
+
+	 router.route("/:id")
+	 .get(auth.decodeToken, controller.FetchOneTransaction);
+
+	 module.exports = router;
