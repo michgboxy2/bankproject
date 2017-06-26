38150adc01b2b1730abdfc88db4bf7b01b7865fa
michael <michgboxy@gmail.com>
2017-06-26 06:25:57 -0700

    integration test to fetch one admin from the database

--- server/specs.js
+++ server/specs.js
@@ -7,10 +7,10 @@ var request = require('supertest'),
 
 	//integration test for the admin endpoints
 	describe("Admin Endpoints", function(){
-		it.skip("should add an admin", function(next) {
+		it("should add an admin", function(next) {
 
 				var data = {
-					'username' : "Michael",
+					'username' : "Michaellllii",
 					'password' : "King"
 				}
 
@@ -21,9 +21,9 @@ var request = require('supertest'),
 						.expect(200)
 						.end(function(err, res) {
 							//expect(res.body).to.be.an("object");
-							expect(res.body.username).to.be.equal("Michael");
+							expect(res.body.username).to.be.equal("Michaellllii");
 							//console.log(res.body);
-							//res.body.should.be.json;
+							res.body.should.be.json;
 							next();
 						})
 
@@ -36,7 +36,18 @@ var request = require('supertest'),
 			.set("Content-Type", "Application/json")
 			.end(function(err, res){
 				res.body.should.be.json;
+				//console.log(res.body);
+
 				next();
+			})
+		})
+		it("should fetch one admin by Id", function(next){
+			request(app)
+			.get("/api/v1/admin"+id)
+			.expect(200)
+			.set("Content-Type", "Application/json")
+			.end(function(err, res){
+				res.body.should.be.json;
 			})
 		})
 	})
