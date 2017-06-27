651cecea6ce68115f8e92888f8e28f52a7d727bd
michael <michgboxy@gmail.com>
2017-06-26 19:52:35 -0700

    using AJAX to consume the API and allow admin to register

--- www/js/app.js
+++ www/js/app.js
@@ -1,62 +1,54 @@
-
 (function(){
 
 	"use strict";
 
-
 	var getElement = document.getElementById.bind(document),
+		regform    = getElement("register"),
+		login	   = getElement("Login"),
 
-	    Register =   getElement('add-contact'),
+		xhr = new XMLHttpRequest();
 
-	    xhr = new XMLHttpRequest();
+		regform.addEventListener('submit', function(e){
+			e.preventDefault();
 
-	    Register.addEventListener('submit', function(e){
-	    	e.preventDefault();
+			var data = "",
+				elements = this.elements;
+				//make arraylike data inherit array property
+			Array.prototype.forEach.call(elements, function(v, i, a){
+				data += encodeURIComponent(v.name);
+				data += "=";
+				data += encodeURIComponent(v.value);
+				data += "&";
+			})
+				//avoid trailing ampersand
+			data = data.substring(0, data.length-1);
 
-	    	var data = "",
-	    		element = this.elements;
+				//link to the API
+			xhr.open("POST", "http://192.168.33.20:3000/api/v1/admin");
 
-	    		Array.prototype.forEach.call(elements, function(v,i,a){
-	    			data+= encodeURIComponent(v.name);
-	    			data+= "_";
-	    			data+= encodeURIComponent(v.value);
-	    			data+="&";
-	    		})
-	    		// avoid trailing ampersand
-	    		data = data.substring(0, data.length-1);
+			xhr.onreadystatechange = function(){
+				handleresponse(xhr);
+			};
 
-	    		//link form with the API
-	    		xhr.open("POST", "http://192.168.33.20:3000/api/v1/admin")
-	    		xhr.onreadystatechange = function(){
-	    			handleresponse(xhr);
-	    		};
+			xhr.setRequestHeader("Content-Type", "Application/x-www-form-urlencoded");
 
-	    		xhr.setRequestHeader("Content-Type", "x-www-form-urlencoded");
-
-	    		xhr.send(data);
-	    }, false);
-
-	    		function handleresponse(http){
-	    			if(http.readyState == 4){
-	    				if(http.status == 200 || http.status == 304){
-	    					var user = JSON.parse(http.responseText);
-	    					var login = getElement("Login");
-
-	    					if(user.hasownproperty(_token)){
-	    						Register.classList.toggle("hide");
-	    					     login.classList.toggle("hide");
-	    					}
-	    				}
-
-	    			}
-	    		}
+			xhr.send(data);
 
 
+		}, false);
 
-
-
-
-
+			function handleresponse(http){
+				if(http.readyState == 4){
+					if(http.status == 200 || http.status == 304){
+						var user = JSON.parse(http.responseText);
+						var loginForm = getElement("Login");
+						if(user.hasOwnProperty("_token")){
+							regform.classList.toggle("hide");
+							loginForm.classList.toggle("hide");
+						}
+					}
+				}
+			}
 
 
 
