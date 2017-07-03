79f63ea182ef58bb601370a5eee28dc4cd4dfa3b
michael <michgboxy@gmail.com>
2017-07-02 22:21:38 -0700

    loop through the json response of a customer search and arrange the display to the user

--- www/js/dashboard.js
+++ www/js/dashboard.js
@@ -5,9 +5,10 @@
 
 		 regform =	 getElement("register"),
 		 view 	 =   getElement("view"),
-		 viewpage =  getElement("viewpage");
+		 viewpage =  getElement("viewpage"),
+		 list     =  getElement("list");
 
-		 console.log(view);
+		 console.log(list);
 		 
 
 
@@ -88,16 +89,17 @@
 		 		
 
 		 		
-
-		 		xhr.open("GET", "http://192.168.33.20:3000/api/v1/customer/"+email.value);
+		 		var url = "http://192.168.33.20:3000/api/v1/customer/"+ String(email.value);
+		 		console.log(url);
+		 		xhr.open("GET", url);
 		 		xhr.onreadystatechage = function(){
 		 			handleview(xhr);
 		 		}
 
-		 		xhr.setRequestHeader("Content-Type", "Application/json");
-		 		xhr.setRequestHeader("Content-Type", "Bearer" + localStorage.getItem("token"));
+		 		//xhr.setRequestHeader("Content-Type", "Application/json");
+		 		xhr.setRequestHeader("Authorization", "Bearer " + localStorage.getItem("token"));
 
-		 		xhr.send();
+		 		xhr.send(null);
 
 		 	}, false);
 
@@ -106,42 +108,26 @@
 		 			if(http.readyState == 4){
 		 				if(http.status == 200 || http.status == 304){
 		 					var user = JSON.parse(http.responseText);
-
-		 					console.log(http.responseText);
-
-		 					var viewcus = getElement("list");
-		 					viewcus.innerHTML = http.responseText;
-
-
-
-		 							 				
+		 						user.forEach(function(user){
+		 							var list = document.getElementById('list'),
+		 								li = document.createElement("li"),
+		 								H = document.createElement("h3");
+		 								H.innerText = user.firstname;
+		 								H.innerText = user.lastname;
+		 								H.innerText = user.email;
+		 								H.innerText = user.account_number;
+		 								H.innerText = user.account_type;
+		 								H.innerText = user.account_balance;
+		 							var	br = document.createElement("br");
+		 								li.appendChild(H);
+		 								li.appendChild(br);
+		 								list.appendChild(li);
+		 					})
 
 
 		 				}
 		 			}
 		 		}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
