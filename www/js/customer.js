0bad1e5c393d670b0c5284b8f9e8f2546250a23e
michael <michgboxy@gmail.com>
2017-07-03 16:59:55 -0700

    customer login ajax page

---
+++ www/js/customer.js
@@ -0,0 +1,51 @@
+(function(){
+
+	"use strict";
+	var getElement = document.getElementById.bind(document),
+
+	 loginform = getElement("login"),
+
+				xhr = new XMLHttpRequest();
+
+				//console.log(loginform);
+
+				loginform.addEventListener('submit', function(e){
+					e.preventDefault();
+
+					var data = {},
+					elements = this.elements;
+					console.log(elements);
+					//foreach loop of the arraylike object
+					Array.prototype.forEach.call(elements, function(v,i,a){
+						//avoid trailing ampersand
+						data[encodeURIComponent(v.name)] = encodeURIComponent(v.value);
+						console.log(data);
+
+						xhr.open("POST", "http://192.168.33.20:3000/api/v1/customerauth");
+						xhr.onreadystatechange = function(){
+							handleLogin(xhr);
+						}
+
+						xhr.setRequestHeader("Content-Type", "Application/json");
+
+						xhr.send(JSON.stringify(data));
+
+					})
+
+				},false);
+
+						function handleLogin(http){
+							if(http.readyState == 4){
+								if(http.status == 200 || http.status == 304){
+									var user = JSON.parse(http.responseText);
+									if(user.hasOwnProperty("_token")){
+										localStorage.setItem("token", user._token);
+										window.location = "homepage.html";
+
+
+									}
+								}
+							}
+						}
+
+}());
