fe0a45de06c2bda0596c51d140e849bf75c52641
michael <michgboxy@gmail.com>
2017-07-04 10:17:09 -0700

    designs the admin model and exports it

--- api/v1/admin/admin-model.js
+++ api/v1/admin/admin-model.js
@@ -1,33 +1,1 @@
-var  mongoose 	= require("mongoose"),
-	 bcrypt		= require("bcrypt-nodejs"),
-	 AdminSchema;
-
-	 mongoose.connect("mongodb://localhost/admin");
-
-	 AdminSchema = new mongoose.Schema({
-	 	username : {type : String, required : true, unique: true},
-	 	password: {type: String, required: true},
-	 	date : {type: Date, default: Date.now}
-	 })
-
-
-	 AdminSchema.pre('save', function(next){
-	 	this.password = this.encryptPassword(this.password);
-	 	next();
-	 })
-
-	 //method to authenticate user
-	 AdminSchema.methods = {
-	 	authenticate : function(plaintext){
-	 		return bcrypt.compareSync(plaintext, this.password);
-	 	},
-
-	 	encryptPassword : (plaintext) => {
-	 		if(!plaintext){ return next(new Error("please enter your password"));}
-	 		var salt = bcrypt.genSaltSync();
-	 		return bcrypt.hashSync(plaintext, salt);
-	 	}
-
-	 }
-
-	 module.exports = mongoose.model("admin", AdminSchema);
+var mongoose = require("mongoose"),	bcrypt   = require("bcrypt-nodejs"),	AdminSchema;	mongoose.connect("mongodb://localhost/admin");	AdminSchema = new mongoose.Schema({		username : {type: String, required : true, unique : true},		password : {type: String, required : true}	});	AdminSchema.pre('save', function(next){		this.password = this.encryptPassword(this.password);		next();	});	AdminSchema.methods = {		authenticate : function(plaintext){			return bcrypt.compareSync(plaintext, this.password);		},		encryptPassword : (plaintext) => {			if(!plaintext){ return next(new Error("please enter username or password"));}			var salt = bcrypt.genSaltSync();			return bcrypt.hashSync(plaintext, salt);		}	}	module.exports = mongoose.model("admin", AdminSchema);
