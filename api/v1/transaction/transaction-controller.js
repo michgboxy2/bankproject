1f694b752ac585d3be71bdeb0ade22d7db536237
michael <michgboxy@gmail.com>
2017-07-04 16:47:35 -0700

    handling funds transfer calculations

--- api/v1/transaction/transaction-controller.js
+++ api/v1/transaction/transaction-controller.js
@@ -9,51 +9,6 @@ var transactionModel = 	require("./transaction-model"),
 			recipient : req.body.recipient
 		})
 
-
-		console.log(req.body.amount);
-
-		customer.findById(req.user._id).then((data) => {
-			if(!data){return next(new Error("can't find customer"));}
-			data = data.toObject();
-
-			var balance = data.account_balance,
-				amount  = req.body.amount;
-			
-			console.log(balance);
-			console.log(amount);
-			if(balance > amount){
-			var total = (balance - amount);
-			} else {
-				return next(new Error("INSUFFICIENT BALANCE"));
-			}
-			
-			
-
-
-
-			
-		}, (err) => { return next(err);})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 		transactionModel.findOne({_id : req.user._id})
 		.populate('accountname', 'firstname')
 		.exec(function(err, transaction){
@@ -62,6 +17,61 @@ var transactionModel = 	require("./transaction-model"),
 
 		})
 
+
+
+
+
+		customer.findById(req.user._id).then((data) => {
+			if(!data){return next(new Error("can't find customer"));}
+			
+				data = data.toObject();
+
+			var balance = data.account_balance,
+				amount  = req.body.amount,
+				
+				total   = (balance - amount);
+
+				data["account_balance"] = total;
+
+
+				customer.findByIdAndUpdate(req.user._id, data).then((customers) => {
+					if(!customers){ return next(new Error("can't update"));}
+
+					console.log(customers);				
+					
+				}, (err) => { return next(err);})
+
+				
+				
+
+			
+
+						
+	
+		}, (err) => { return next(err);})
+		
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		
 
 
 
